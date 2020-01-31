8614eb2ea0e880a2aac85cf266f6d3f6de9a4807
matekuna <kuna.mate@parlament.hu>
2020-01-31 10:06:00 +0100

    getEmployees function, html table added

--- index.js
+++ index.js
@@ -3,17 +3,74 @@ const express = require('express');
 const http = require('http');
 const winston = require('winston');
 const oracledb = require('oracledb');
+const socketio = require('socket.io');
+const dbConfig = require('./server/dbconfig');
+///const serveStatic = require('serve-static');
 
 const app = express();
-app.listen(8080);
-const PORT = process.env.PORT || 3000;
+const httpserver = http.Server(app);
+const io = socketio(httpserver);
+const PORT = process.env.PORT || 8080;
+
+app.listen(PORT);
+
+/// app.use('/', serveStatic(__dirname + '/public'));
 
 app.get('/', (req, res) => {
-    res.end('Hello World!');
     winston.debug(`Your port is ${PORT}`);
+    res.sendFile(__dirname + '/public/index.html'); // serve-static package is erre való, a sendFile beépített function
+    // res.end('Hello World!');
 });
 
+app.get('/api/employees', (req, res, next) => {
+    getEmployees(req, res, next, 'API');
+})
 
+// valamennyi dolgozó lekérdezése oracledb driverrel
+function getEmployees(req, res, next, context)
+{
+    oracledb.getConnection(
+        dbConfig,
+        
+        function(err, connection)
+        {
+            if(err)
+            {
+                return next(err);
+            }
+
+            connection.execute(
+                'SELECT * FROM EMPLOYEES',
+                {}, // bind paraméter most üresen marad
+                {
+                    outFormat: oracledb.OBJECT
+                },
+                function(err, results) 
+                {
+                    if (err) 
+                    {
+                        return connection.release(function() {
+                            next(err);
+                        });
+                    }
+                    connection.release(function(err) 
+                    {
+                        if (err) return next(err);
+
+                        if (context === 'API') 
+                        {
+                            res.send(results.rows); console.log(results.rows);
+                        } else if (context === 'SOCKET_IO') 
+                        {
+                            io.emit('change', results.rows);
+                            res.send();
+                        }
+                    });
+                }
+            )
+        }
+    );
+}
 
 
 /*
