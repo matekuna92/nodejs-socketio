fcabb51858ea236cf78674967186ad8b1de1861b
matekuna <kuna.mate@parlament.hu>
2020-02-04 13:14:36 +0100

    emit event on button click

--- index.js
+++ index.js
@@ -19,16 +19,117 @@ console.log(PORT);
 io.on('connection', socket => {
     console.log('a user connected');
     
-    socket.on('disconnect', function() {
+    socket.on('employee added', function()
+    {
+        console.log('button clicked!');   
+        
+        /*
+        try
+        {
+        //    socket.emit('employee added');
+            /*
+            let connection = oracledb.getConnection(dbConfig);
+            connection.execute(
+                `INSERT INTO EMPLOYEES(:ID, :DEPARTMENT_ID, :NAME, :EMAIL, :COST_CENTER, :DATE_HIRED, :JOB) 
+                    VALUES(20001014041041, 209119519135897625115199142974062211758, 'USER 5', 'newest@mail.com', 55,
+                    TO_DATE('2020-01-01','YYYY-MM-DD'), 'Developer')`,
+            );
+            oracledb.autoCommit = true;
+            console.log("Rows inserted: " + result.rowsAffected);
+            */
+
+           /// io.sockets.emit('employee added', emp);
+
+           /**** működik!
+            socket.emit('newMessage', 
+            { 
+                from:'jen@mds', 
+                text:'hepppp', 
+                createdAt:123 
+            }); 
+            
+            socket.on('createMessage', (newMessage)=>{ 
+                console.log('newMessage', newMessage); 
+            });
+            
+        }
+        catch(err)
+        {
+            if(err)
+            {
+                console.log(err);
+            }
+        }
+        */
+        
+    })
+
+    /*
+    // add employee esemény figyelése
+    socket.on('add employee', function(emp) 
+    {
+        add_employee(emp, function(res)
+        {
+            if(res)
+            {
+                console.log('employee added in funciton');
+                io.emit('employee added', emp);
+            }
+            else
+            {
+                console.log('error, cant add employee');
+                io.emit('error');
+            }
+        })
+    })
+    */
+
+    socket.on('disconnect', function() 
+    {
       console.log('user disconnected');
     });
   });
 
+/*****
+let add_employee = function(status, callback)
+{
+    oracledb.getConnection(dbConfig, function(err, connection)
+    {
+        if(err)
+        {
+            callback(false);
+            console.log('ERROR in add_employee');
+            return;
+        }
+        
+        connection.query("INSERT INTO EMPLOYEES(ID, NAME, LOCATION, COUNTRY) VALUES('5', 'Newest User', 'Test City', 'Germany')", 
+            function(err,rows)
+            {
+                console.log('CONNETION QUERY OK');
+                connection.release();
+                if(!err) {
+                  callback(true);
+                }
+            });
+
+        connection.on('error', function(err) 
+        {
+            console.log('ERROR IN QUERY');
+            callback(false);
+            return;
+        });
+    })
+}
+*****/
+
+
+/*
 io.on('change', function(employees)
 {
     console.log('DB changed!');
   //  showEmployees(employees);
 });
+*/
 
 /// app.use('/', serveStatic(__dirname + '/public'));
 
@@ -90,6 +191,25 @@ function getEmployees(req, res, next, context)
     );
 }
 
+/*
+async function addEmployee(req, res)
+{
+    try
+    {
+        const connection = await oracledb.getConnection(dbConfig);
+        connection.execute(
+            `INSERT INTO EMPLOYEES(:ID, :DEPARTMENT_ID, :NAME, :EMAIL, :COST_CENTER, :DATE_HIRED, :JOB) 
+                VALUES(20001014041041, 209119519135897625115199142974062211758, 'USER 5', 'newest@mail.com', 55,
+                TO_DATE('2020-01-01','YYYY-MM-DD'), 'Developer');`,
+        );
+        console.log("Rows inserted: " + result.rowsAffected);
+    }
+    catch (err) 
+    {
+        console.error(err);
+    }       
+}
+*/
 
 /*
 let server = http.createServer(function(req, res) {
